63fa65a73422751364ac527926cd831ed8c7bd59
Mirek Nguyen <mireknguyenbinh@seznam.cz>
2024-01-07 16:50:40 +0100

    feat: release class

---
+++ github_rss/release.py
@@ -0,0 +1,36 @@
+import requests
+
+class Release:
+    def latest_release(self, repository, headers):
+        response = requests.get(
+            f"https://api.github.com/repos/{repository}/releases/latest", headers=headers
+        )
+        if response.status_code == 404:
+            return {
+                "error": True,
+                "message": f"{response.status_code}: Repository release not found.",
+            }
+        if response.status_code == 403:
+            return {
+                "error": True,
+                "message": f"{response.status_code}: API rate limit exceeded.",
+            }
+        if response.status_code != 200:
+            return {"error": True, "message": f"{response.status_code}: Unknown error."}
+        return response.json()
+
+
+    def latest_release_list(self, repository_list, headers):
+        details = []
+        for release in repository_list:
+            repository_release = self.latest_release(release, headers)
+            if repository_release.get("error"):
+                continue
+            release = {
+                "name": release,
+                "tag_name": repository_release["tag_name"],
+                "published_at": repository_release["published_at"],
+            }
+            details.append(release)
+        sorted_details = sorted(details, key=lambda x: x["published_at"], reverse=True)
+        return sorted_details
