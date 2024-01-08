f3b1d08b755929c9780d4ecd3bd4ef5da605d6b7
Mirek Nguyen <mireknguyenbinh@seznam.cz>
2024-01-08 15:00:58 +0100

    feat: tools sort list

--- github_rss/tools.py
+++ github_rss/tools.py
@@ -8,3 +8,6 @@ class Tools:
     def get_limits(headers):
         response = requests.get("https://api.github.com/rate_limit", headers=headers, timeout=5)
         return response.json()
+    @staticmethod
+    def sort_list_by(list, key, reverse=True):
+        return sorted(list, key=lambda x: x[key], reverse=reverse)
