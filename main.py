14160fdeb0caae6e42115d010a4c180effef0071
Mirek Nguyen <mireknguyenbinh@seznam.cz>
2024-01-08 15:02:38 +0100

    feat: use classes as separate files

--- main.py
+++ main.py
@@ -1,82 +1,43 @@
+import os
 import sys
 
-import requests
 from dotenv import load_dotenv
-import os
+
+from github_rss.release import Release
+from github_rss.repository import Repository
+from github_rss.tools import Tools
 
 load_dotenv()
-GITHUB_TOKEN=os.getenv("GITHUB_TOKEN")
-DEFAULT_HEADERS = {
-    'Authorization': f'token {GITHUB_TOKEN}',
-    'Accept': 'application/vnd.github.v3+json',
-}
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 if GITHUB_TOKEN is None:
     print("GITHUB_TOKEN is not set.")
     sys.exit(1)
-def latest_release(repository, headers = DEFAULT_HEADERS):
-    response = requests.get(
-        f"https://api.github.com/repos/{repository}/releases/latest",
-        headers=headers
-    )
-    if response.status_code == 404:
-        return {"error": True, "message": f"{response.status_code}: Repository release not found."}
-    if response.status_code == 403:
-        return {"error": True, "message": f"{response.status_code}: API rate limit exceeded."}
-    if response.status_code != 200:
-        return {"error": True, "message": f"{response.status_code}: Unknown error."}
-    return response.json()
 
-def get_limits(headers = DEFAULT_HEADERS):
-    response = requests.get("https://api.github.com/rate_limit", headers=headers)
-    return response.json()
+DEFAULT_HEADERS = {
+    "Authorization": f"token {GITHUB_TOKEN}",
+    "Accept": "application/vnd.github.v3+json",
+}
 
-def starred_repositories(username, headers = DEFAULT_HEADERS):
-    starred = []
-    page = 1
-    per_page = 100
-    while True:
-        params = {
-            'page': page,
-            'per_page': per_page,
-        }
-        response = requests.get(
-            f"https://api.github.com/users/{username}/starred",
-            headers=headers,
-            params=params,
-        )
-        if response.status_code != 200:
-            return {"error": True, "message": f"{response.status_code}: Error getting starred repositories"}
-        current_page = response.json()
-        starred.extend(current_page)
-        if len(current_page) < per_page:
-            break
-        page += 1
+release = Release()
+repository = Repository()
+starred = repository.starred_repositories("mireknguyen", DEFAULT_HEADERS)
 
-    details = []
-    for release in starred:
-        repository_release = latest_release(release['full_name'])
-        if repository_release.get("error"):
-            continue
-        release = {
-            'name': release['full_name'],
-            'tag_name': repository_release['tag_name'],
-            'published_at': repository_release['published_at'],
-        }
-        details.append(release)
-    sorted_details = sorted(details, key=lambda x: x['published_at'], reverse=True)
-    return sorted_details
-    # return details
+releases = release.latest_release_list(starred, DEFAULT_HEADERS)
+sorted_releases = Tools.sort_list_by(releases, "published_at")
+print(sorted_releases)
+# print(Tools.get_limits(DEFAULT_HEADERS)["rate"])
 
-
-def print_latest_release(data):
-    print(data["html_url"])
-    print(data["published_at"])
-    print(data["tag_name"])
-    print(data["name"])
-    print(data["prerelease"])
-    print(data["body"])
-
-print(starred_repositories("mireknguyen"))
+# print(release.latest_release("mireknguyen/mirekng-homepage", DEFAULT_HEADERS))
+# repository.starred_repositories("mireknguyen", DEFAULT_HEADERS)
+# print(starred_repositories("mireknguyen"))
 # print(latest_release("mireknguyen/mirekng-homepage"))
-limits = get_limits()
-print(limits['rate'])
+
+# fe = {
+#     id: id,
+#     title: title,
+#     link: link,
+#     description: description,
+#     pubDate: pub_date,
+# }
+# fe_list = [ fe1, fe2, fe3]
+
